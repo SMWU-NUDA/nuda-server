d33f79c8ead911d196f750775cea71baa1f7b957
happine2s <happine2s@sookmyung.ac.kr>
2025-12-26 20:32:21 +0900

    Feat: 스프링부트 프로젝트 세팅

---
+++ src/main/java/smu/nuda/NudaApplication.java
@@ -0,0 +1,13 @@
+package smu.nuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smu/nuda/NudaApplicationTests.java
@@ -0,0 +1,13 @@
+package smu.nuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
