f6a1badefa494cb506cfe54e62eac3e51dd063bf
happine2s <happine2s@sookmyung.ac.kr>
2025-12-28 16:17:06 +0900

    Feat: swagger 설정

---
+++ src/main/java/smu/nuda/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package smu.nuda.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "NUDA API",
+                description = "NUDA 서비스 백엔드 API 문서",
+                version = "v1"
+        )
+)
+@SecurityScheme(
+        name = "JWT",
+        description = "Example: \"Bearer {token}\"",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
