bc8206378c5744d7bec405700768a4ba3b3acc22
happine2s <happine2s@sookmyung.ac.kr>
2026-01-09 06:03:17 +0900

    Feat: 리뷰 관련 sql 추가

---
+++ src/main/resources/db/migration/V6.3__create_review_like.sql
@@ -0,0 +1,15 @@
+CREATE TABLE review_like (
+    id BIGINT PRIMARY KEY DEFAULT nextval('review_like_seq'),
+    review_id BIGINT NOT NULL,
+    CONSTRAINT fk_review_like_review
+        FOREIGN KEY (review_id) REFERENCES review(id) ON DELETE CASCADE,
+    member_id BIGINT NOT NULL,
+    CONSTRAINT fk_review_like_member
+        FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+
+    CONSTRAINT uq_review_like_unique UNIQUE (review_id, member_id)
+);
+
+CREATE INDEX idx_review_like_review_member ON review_like(review_id, member_id);
