a863b3ae47bc23af93eeb31f1204a0146cc4a329
happine2s <happine2s@sookmyung.ac.kr>
2026-02-07 03:30:06 +0900

    Feat: 멤버 배송지 vo 정적 팩토리 메소드 구현

--- src/main/java/smu/nuda/domain/member/dto/DeliveryResponse.java
+++ src/main/java/smu/nuda/domain/member/dto/DeliveryResponse.java
@@ -1,14 +1,26 @@
 package smu.nuda.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import smu.nuda.domain.member.entity.Member;
 
 @Getter
-@Builder
+@AllArgsConstructor
 public class DeliveryResponse {
     private String recipient;
     private String phoneNum;
     private String postalCode;
     private String address1;
     private String address2;
+
+    public static DeliveryResponse from(Member member) {
+        return new DeliveryResponse(
+                member.getRecipient(),
+                member.getPhoneNum(),
+                member.getPostalCode(),
+                member.getAddress1(),
+                member.getAddress2()
+        );
+    }
 }
