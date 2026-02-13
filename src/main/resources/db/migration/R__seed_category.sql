3088b1b06b0b9dfd95394737d6e7b4a8071bf028
happine2s <happine2s@sookmyung.ac.kr>
2026-02-13 19:51:47 +0900

    Feat: 실제 데이터에 맞춘 카테고리 sql 추가

--- src/main/resources/db/migration/R__seed_category.sql
+++ src/main/resources/db/migration/R__seed_category.sql
@@ -1,8 +1,16 @@
 INSERT INTO category (name, code, description)
 VALUES
-    ('소형', 'SMALL', '패드형 소형'),
-    ('중형', 'MEDIUM', '패드형 중형'),
-    ('대형', 'LARGE', '패드형 대형'),
-    ('오버나이트', 'OVERNIGHT', '패드형 오버나이트'),
-    ('팬티라이너', 'LINER', '팬티라이너'),
-    ('탐폰', 'TAMPON', '탐폰형');
+    ('입는거 소형', 'PUTSMALL', '팬티형 소형'),
+    ('입는거 중형', 'PUTMEDIUM', '팬티형 중형'),
+    ('입는거 대형', 'PUTLARGE', '팬티형 대형'),
+    ('입는거 오버나이트', 'PUTOVERNIGHT', '팬티형 오버나이트'),
+
+    ('탐폰 소형', 'TAMPOSMALL', '탐폰 소형'),
+    ('탐폰 중형', 'TAMPOMEDIUM', '탐폰 중형'),
+    ('탐폰 대형', 'TAMPOLARGE', '탐폰 대형'),
+
+    ('팬티라이너', 'LINER', '패드형 팬티라이너'),
+    ('패드 소형', 'SMALL', '패드형 소형'),
+    ('패드 중형', 'MEDIUM', '패드형 중형'),
+    ('패드 대형', 'LARGE', '패드형 대형'),
+    ('패드 오버나이트', 'OVERNIGHT', '패드형 오버나이트');
