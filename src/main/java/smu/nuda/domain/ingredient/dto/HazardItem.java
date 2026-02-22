a6aeac5a67970e966142d9e6ee8478cf27655df5
happine2s <happine2s@sookmyung.ac.kr>
2026-02-23 05:44:03 +0900

    Feat: 성분 상세조회 응답 dto 구성

---
+++ src/main/java/smu/nuda/domain/ingredient/dto/HazardItem.java
@@ -0,0 +1,11 @@
+package smu.nuda.domain.ingredient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HazardItem {
+    private String code;
+    private String description;
+}
