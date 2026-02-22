7c4a5f9b98f1bfb1e7b1859e51e0e66bf45a5136
happine2s <happine2s@sookmyung.ac.kr>
2026-02-22 22:15:36 +0900

    Feat: 로그 jpa 엔티티 구현

---
+++ src/main/java/smu/nuda/domain/log/entity/CommerceLog.java
@@ -0,0 +1,49 @@
+package smu.nuda.domain.log.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import smu.nuda.domain.log.entity.enums.CommerceType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rec_event_log")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CommerceLog {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "rec_event_log_seq_generator")
+    @SequenceGenerator(
+            name = "rec_event_log_seq_generator",
+            sequenceName = "rec_event_log_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "external_product_id", nullable = false)
+    private String externalProductId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_type", nullable = false)
+    private CommerceType commerceType;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(name = "occurred_at", nullable = false)
+    private LocalDateTime occurredAt;
+
+}
