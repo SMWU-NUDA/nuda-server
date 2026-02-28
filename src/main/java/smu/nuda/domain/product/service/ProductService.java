2b37cdabbaa4d43317377ea4e590b81cccf182a4
happine2s <happine2s@sookmyung.ac.kr>
2026-03-01 00:17:25 +0900

    Feat: 상품 상세 조회 시 캐시 파사드 적용

--- src/main/java/smu/nuda/domain/product/service/ProductService.java
+++ src/main/java/smu/nuda/domain/product/service/ProductService.java
@@ -7,51 +7,27 @@ import org.springframework.transaction.annotation.Transactional;
 import smu.nuda.domain.common.dto.Cursor;
 import smu.nuda.domain.common.dto.CursorResponse;
 import smu.nuda.domain.like.repository.BrandLikeRepository;
+import smu.nuda.domain.product.cache.ProductCacheFacade;
 import smu.nuda.domain.product.dto.ProductDetailCache;
 import smu.nuda.domain.product.dto.ProductDetailResponse;
 import smu.nuda.domain.product.dto.ProductItem;
 import smu.nuda.domain.product.dto.enums.ProductSortType;
-import smu.nuda.domain.product.error.ProductErrorCode;
-import smu.nuda.domain.product.repository.ProductImageQueryRepository;
 import smu.nuda.domain.product.repository.ProductQueryRepository;
 import smu.nuda.domain.like.repository.ProductLikeRepository;
-import smu.nuda.global.error.DomainException;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
+    private final ProductCacheFacade productCacheFacade;
     private final ProductQueryRepository productQueryRepository;
-    private final ProductImageQueryRepository productImageQueryRepository;
     private final ProductLikeRepository productLikeRepository;
     private final BrandLikeRepository brandLikeRepository;
 
-    @Cacheable(value = "productDetail", key = "#productId")
-    @Transactional(readOnly = true)
-    public ProductDetailCache getProductDetailCache(Long productId) {
-
-        ProductDetailCache base = productQueryRepository.findProductBase(productId);
-        if (base == null) throw new DomainException(ProductErrorCode.INVALID_PRODUCT);
-        List<String> imageUrls = productImageQueryRepository.findImageUrlsByProductId(productId);
-
-        return ProductDetailCache.builder()
-                .productId(base.getProductId())
-                .brandId(base.getBrandId())
-                .brandName(base.getBrandName())
-                .name(base.getName())
-                .averageRating(base.getAverageRating())
-                .reviewCount(base.getReviewCount())
-                .price(base.getPrice())
-                .content(base.getContent())
-                .imageUrls(imageUrls)
-                .build();
-    }
-
     @Transactional(readOnly = true)
     public ProductDetailResponse getProductDetail(Long productId, Long memberId) {
-        ProductDetailCache cache = getProductDetailCache(productId);
+        ProductDetailCache cache = productCacheFacade.getProductDetail(productId);
+
         boolean productLikedByMe = productLikeRepository.existsByMember_IdAndProduct_Id(memberId, productId);
         boolean brandLikedByMe = brandLikeRepository.existsByMember_IdAndBrand_Id(memberId, cache.getBrandId());
 
